8e92acaf2ed9216e2eb8e46f0f29abcc55ecefac
Prof Pierce <piercen@alfredstate.edu>
2024-11-11 13:22:02 +0000

    imported all files

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "node-mongo-webserver",
+  "version": "1.0.0",
+  "description": "A Node.js web server with MongoDB integration",
+  "main": "server.js",
+  "scripts": {
+    "start": "nodemon server.js"
+  },
+  "dependencies": {
+    "bcrypt": "^5.1.1",
+    "dotenv": "^16.4.5",
+    "ejs": "^3.1.9",
+    "express": "^4.19.2",
+    "mongoose": "^8.3.1",
+    "multer": "^1.4.5-lts.1",
+    "nodemon": "^3.1.0",
+    "pug": "^3.0.2"
+  },
+  "author": "Your Name",
+  "license": "ISC"
+}
